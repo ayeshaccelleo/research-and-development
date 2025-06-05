d69db72befccfb64e6237cedf129e33afc837872
ayeshaccelleo <ayesh.bamunuarachchi@accelleo.com>
2025-06-05 10:06:11 +0530

    check ci/cd

--- src/components/common-charts/area-charts/AreaCharts.tsx
+++ src/components/common-charts/area-charts/AreaCharts.tsx
@@ -5,7 +5,7 @@ const AreaCharts = () => {
   return (
     <>
     <div className="flex flex-row justify-start ">
-      <h2 className="font-bold mb-4">Area Charts</h2>
+      <h2 className="font-bold mb-4">Area Charts - Charts we ca Implement in CANVAS </h2>
     </div>
       <div className="grid grid-cols-3 grid-rows-1 gap-4">
         <AreaChartCurved />
